a0936e81b6d99e21925e50697ea2e45e37cd74b6
igor-graca <80965280+igor-graca@users.noreply.github.com>
2026-01-27 08:39:08 +0100

    Release 11.3.0 (#22)

--- MAUI/Platforms/Android/MainActivity.cs
+++ MAUI/Platforms/Android/MainActivity.cs
@@ -1,12 +1,10 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using Anyline.SDK.NET.Android;
 
 namespace Anyline.Examples.MAUI;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, EnableOnBackInvokedCallback = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     public static Activity Instance;
